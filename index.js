a4d59da81245661b381ee4d94bc0e100fc70e1c9
mmm_js <mmm_js@naver.com>
2020-07-03 01:06:09 +0900

    ES6 on NodeJS using Babel

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hi!");
+import "core-js";
+import express from "express";
+const app = express();
+
+const PORT = 4000;
+const handleListening = () =>
+  console.log(`Listening on : http://localhost:${PORT}`);
+
+const handleHome = (req, res) => res.send("hello");
+
+const handleProfile = (req, res) => res.send("you");
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
