17c12c120d0ad724212505b87b1232823b61ed69
mmm_js <mmm_js@naver.com>
2020-07-04 00:17:45 +0900

    MVC Pattern part One
    create routers

--- app.js
+++ app.js
@@ -4,13 +4,11 @@ import morgan from "morgan";
 import helmet from "helmet";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
-import { userRouter } from "./router.js";
+import globalRouter from "./routers/globalRouter.js";
+import userRouter from "./routers/userRouter.js";
+import videoRouter from "./routers/videoRouter.js";
 
 const app = express();
-
-const handleHome = (req, res) => res.send("hello");
-
-const handleProfile = (req, res) => res.send("you! hi");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -18,9 +16,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet()); //보안에 도움이 됨
 app.use(morgan("dev")); //logging
 
-app.get("/", handleHome);
-app.get("/profile", handleProfile);
-
+app.use("/", globalRouter);
 app.use("/user", userRouter);
+app.use("/video", videoRouter);
 
 export default app;
