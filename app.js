41287c5060e2497b4d9b4c5e95b4d1b26c516fcc
mmm_js <mmm_js@naver.com>
2020-07-04 18:21:49 +0900

    Local Variables in Pug
    res.locals

--- app.js
+++ app.js
@@ -8,15 +8,18 @@ import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import routes from "./routes";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
-app.set("view engine", "pug");
 
+app.use(helmet()); //보안에 도움이 됨
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet()); //보안에 도움이 됨
 app.use(morgan("dev")); //logging
+
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
