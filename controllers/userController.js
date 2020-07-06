49caf68fd3253ab66def17073d80aa2f206a915d
mmm_js <mmm_js@naver.com>
2020-07-07 01:33:21 +0900

    More Controller
    More Controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -24,7 +24,10 @@ export const postLogin = (req, res) => {
   res.redirect(routes.home);
 };
 
-export const logout = (req, res) => res.render("logout");
+export const logout = (req, res) => {
+  //To Do: Process Log Out
+  res.redirect(routes.home);
+};
 export const userDetail = (req, res) =>
   res.render("userDetail", { pageTitle: "User Detail" });
 export const editProfile = (req, res) =>
