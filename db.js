7e3d9e1b284ba7330da66d144b84e988568efbba
mmm_js <mmm_js@naver.com>
2020-07-07 20:02:30 +0900

    Connecting to MongoDB
    Connecting to MongoDB

--- db.js
+++ db.js
@@ -1,41 +1,19 @@
-export const videos = [
-  {
-    id: 324393,
-    title: "Videow awesome",
-    description: "This is something I love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "mjs",
-      email: "teset@test.com",
-    },
-  },
-  {
-    id: 123241,
-    title: "Videow super",
-    description: "This is something I love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "mjs",
-      email: "teset@test.com",
-    },
-  },
-  {
-    id: 222341,
-    title: "Videow nice",
-    description: "This is something I love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "mjs",
-      email: "teset@test.com",
-    },
-  },
-];
+import mongoose from "mongoose";
+
+//mongod > port=27017
+//mongodb://localhost:포트번호/database이름
+mongoose.connect("mongodb://localhost:27017/wetube", {
+  //configuration(환경설정)
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✔ Connected to DB");
+const handleError = (error) =>
+  console.log(`👹 Error on DB Connection:${error}`);
+
+db.once("open", handleOpen); //DB연결시 한번 호출
+db.on("error", handleError); //error발생시 함수 호출
