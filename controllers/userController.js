d31116052d3e5bee94313777049806c2f0bd476e
mmm_js <mmm_js@naver.com>
2020-10-19 19:17:34 +0900

    Change Password

--- controllers/userController.js
+++ controllers/userController.js
@@ -142,9 +142,28 @@ export const postEditProfile = async (req, res) => {
     });
     res.redirect(routes.me);
   }catch(error){
-    res.redder("editProfile",{pageTitle: "Edit Profile"});
+    res.redirect(routes.editProfile);
   }
 }
 
-export const changePassword = (req, res) =>
+export const getChangePassword = (req, res) =>
   res.render("changePassword", { pageTitle: "Change Password" });
+
+
+export const postChangePassword = async(req, res) => {
+  const {
+    body : { oldPassword, newPassword, newPassword1}
+  } = req;
+  try{
+    if(newPassword !== newPassword1){
+      res.stauts(400).send('No');
+      res.redircet(`/users${routes.changePassword}`);
+      return;
+    }
+    await req.user.changePassword(oldPassword, newPassword);
+    res.redirect(routes.me);
+  }catch(error){
+    res.stauts(400);
+    res.redirect(`/users${routes.changePassword}`);
+  }
+}
